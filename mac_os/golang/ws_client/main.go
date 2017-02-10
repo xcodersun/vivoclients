89cd651c951e1dfdd4731fb0b74f7b39e3f5b779
asxcoder <xcodersun@gmail.com>
2017-02-09 23:48:31 -0800

    To support config file

--- mac_os/golang/ws_client/main.go
+++ mac_os/golang/ws_client/main.go
@@ -6,12 +6,21 @@ import (
 	"os"
 	"os/signal"
 	"time"
+	"io/ioutil"
+	"encoding/json"
 	"github.com/gorilla/websocket"
+	"fmt"
 )
 
-var origin = "http://localhost/"
-
-var url = "ws://localhost:8081/channels/dNPbBY7op5zxqLen/devices/mactest1/ws?room=garage&floor=1st"
+type Config struct {
+	Host        string             `json:"host"`
+	Port        string             `json:"port"`
+	Channel     string             `json:"channel"`
+	Device      string             `json:"device"`
+	Tags        map[string]string  `json:"tags"`
+	AccessToken string             `json:"access_token"`
+	Interval    int                `json:"interval"`
+}
 
 func main() {
 	log.SetFlags(0)
@@ -19,10 +28,12 @@ func main() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
+	config := getConfig()
+	url := getUrl(&config)
 	log.Printf("connecting to %s", url)
 
 	header := http.Header(make(map[string][]string))
-	header.Add("AccessToken", "1234abcd")
+	header.Add("AccessToken", config.AccessToken)
 
 	ws, _, err := websocket.DefaultDialer.Dial(url, header)
 	if err != nil {
@@ -51,17 +62,21 @@ func main() {
 		}
 	}()
 
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
+	ticker1 := time.NewTicker(time.Duration(10)*time.Second)
+	ticker2 := time.NewTicker(time.Duration(config.Interval)*time.Second)
+	defer ticker1.Stop()
+	defer ticker2.Stop()
 
 	for {
 		select {
-		case <-ticker.C:
+		case <-ticker1.C:
 			err := ws.WriteMessage(websocket.PingMessage, []byte{})
 			if err != nil {
 				log.Println("write:", err)
 				return
 			}
+		case <-ticker2.C:
+			log.Println("ticker2")
 		case <-interrupt:
 			log.Println("interrupt")
 			err := ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
@@ -77,4 +92,28 @@ func main() {
 			return
 		}
 	}
+}
+
+func getConfig() Config {
+	raw, err := ioutil.ReadFile("./config/example.json")
+	if err != nil {
+		log.Println(err.Error())
+		os.Exit(1)
+	}
+	var config Config
+	json.Unmarshal(raw, &config)
+	return config
+}
+
+func getUrl(config *Config) string {
+	var url = "ws://%s:%s/channels/%s/devices/%s/ws?"
+	url = fmt.Sprintf(url, config.Host, config.Port, config.Channel, config.Device)
+	if len(config.Tags) != 0 {
+		for k, v := range config.Tags {
+			url += k + "=" + v + "&"
+		}
+		sz := len(url)
+		url = url[:sz-1]
+	}
+	return url
 }
